e9521e594a3a80d31f14e141dffe6d2b482ab465
Hendra Manudinata <hendra@manudinata.me>
2022-02-12 04:11:38 +0000

    Add extras for r8qxx-user 12 SP1A.210812.016 G780GXXS3BVA5 release-keys
    Signed-off-by: Hendra Manudinata <hendra@manudinata.me>

---
+++ README.md
@@ -0,0 +1,21 @@
+## r8qxx-user 12 SP1A.210812.016 G780GXXS3BVA5 release-keys
+- Manufacturer: samsung
+- Platform: kona
+- Codename: r8q
+- Brand: samsung
+- Flavor: r8qxx-user
+- Release Version: 12
+- Id: SP1A.210812.016
+- Incremental: G780GXXS3BVA5
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: samsung/r8qxx/r8q:11/RP1A.200720.012/G780GXXS3BVA5:user/release-keys
+- OTA version: 
+- Branch: r8qxx-user-12-SP1A.210812.016-G780GXXS3BVA5-release-keys
+- Repo: samsung_r8q_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null >> system/system/priv-app/AREmoji/AREmoji.apk
+rm -f system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null >> system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk
+rm -f system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null
+cat system/system/priv-app/DualOutFocusViewer_S/DualOutFocusViewer_S.apk.* 2>/dev/null >> system/system/priv-app/DualOutFocusViewer_S/DualOutFocusViewer_S.apk
+rm -f system/system/priv-app/DualOutFocusViewer_S/DualOutFocusViewer_S.apk.* 2>/dev/null
+cat system/system/priv-app/YourPhone_P1_5/YourPhone_P1_5.apk.* 2>/dev/null >> system/system/priv-app/YourPhone_P1_5/YourPhone_P1_5.apk
+rm -f system/system/priv-app/YourPhone_P1_5/YourPhone_P1_5.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungSmartSuggestions/SamsungSmartSuggestions.apk.* 2>/dev/null >> system/system/priv-app/SamsungSmartSuggestions/SamsungSmartSuggestions.apk
+rm -f system/system/priv-app/SamsungSmartSuggestions/SamsungSmartSuggestions.apk.* 2>/dev/null
+cat system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null >> system/system/app/HoneyBoard/HoneyBoard.apk
+rm -f system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null
+cat system/system/framework/framework-res.apk.* 2>/dev/null >> system/system/framework/framework-res.apk
+rm -f system/system/framework/framework-res.apk.* 2>/dev/null
+cat system/system/framework/oat/arm64/services.odex.* 2>/dev/null >> system/system/framework/oat/arm64/services.odex
+rm -f system/system/framework/oat/arm64/services.odex.* 2>/dev/null
